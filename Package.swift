315a739cca01d071c8661b3e8bd26ccdf6b2d2f3
Nimbus Deploy <nimbus-deploy@nimbus.co>
2026-08-19 16:28:00 +0000

    Set minimum iOS version to 15

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 var package = Package(
     name: "NimbusVungleKit",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
            name: "NimbusVungleKit",
